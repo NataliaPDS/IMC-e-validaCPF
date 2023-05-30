0136c26c09ee74b6e712358cb72edba1bc40a5cf
Natalia <natpereira9102@gmail.com>
2023-05-30 13:45:41 -0400

    Alteração na conexão

--- 2 - Aula/Validacoes.cs
+++ 2 - Aula/Validacoes.cs
@@ -7,7 +7,7 @@ public class Validacoes
 { 
     public static bool ValidaCPF(string cpf)
     {
-
+        //alteração para verificar git ou github
         //primeira parte - tirar pontos e traços
         cpf = cpf.Replace(".", "");//remove ponto do cpf
         cpf = cpf.Replace("-", "");
